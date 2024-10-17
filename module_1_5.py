84437ec2e81c0a92c38390a841b692902bdbb97a
RuslanShum <schumailov.ruslan@yandex.ru>
2024-10-17 19:39:57 +0400

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,6 @@
 immutable_var_=(1,2,'A','B')# неизменяемый тип данных
 print(immutable_var_)
+#immutable_var_[1]=3
 mutable_list_=[1,2,'a','b','c']
 mutable_list_[4]=('Modified')
 print(mutable_list_)
